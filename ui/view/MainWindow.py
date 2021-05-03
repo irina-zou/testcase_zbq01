9fed6192c5f181e1d5396b1b55b9f65b3e8751a8
01376019 <gradyjiang@sfmail.sf-express.com>
2021-05-03 21:25:03 +0800

    select doc

--- ui/view/MainWindow.py
+++ ui/view/MainWindow.py
@@ -10,6 +10,3 @@ class MainWindow(object):
     def __init__(self):
         self.ui = QUiLoader().load(UI_XML_DIR + 'MainWindow.ui')
         self.viewModel = MainWindowViewModel.MainWindowViewModel(self)
-        self.ui.selectButton.clicked.connect(self.viewModel.handle_select_button_click)
-        self.ui.actionButton.clicked.connect(self.viewModel.handle_action_button_click)
-        self.ui.exportButton.clicked.connect(self.viewModel.handle_export_button_click)
--- ui/viewmodel/MainWindowViewModel.py
+++ ui/viewmodel/MainWindowViewModel.py
@@ -1,3 +1,5 @@
+from PySide2.QtWidgets import QFileDialog
+
 import ui.view.MainWindow as MainWindow
 
 
@@ -5,9 +7,15 @@ class MainWindowViewModel(object):
 
     def __init__(self, view: MainWindow.MainWindow):
         self.view = view
+        self.ui = view.ui
+        self.view.ui.selectButton.clicked.connect(self.handle_select_button_click)
+        self.view.ui.actionButton.clicked.connect(self.handle_action_button_click)
+        self.view.ui.exportButton.clicked.connect(self.handle_export_button_click)
 
     def handle_select_button_click(self):
         print("select button click")
+        file_path, file_type = QFileDialog.getOpenFileName(self.view.ui, "选择文件路径")
+        self.ui.pathText.setText(file_path)
 
     def handle_action_button_click(self):
         print("action button click")
