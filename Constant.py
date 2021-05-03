c9d77860fe59fba1fe4f7ba62a2826748b644f04
01376019 <gradyjiang@sfmail.sf-express.com>
2021-05-03 15:49:46 +0800

    resource dir add

--- Constant.py
+++ Constant.py
@@ -9,7 +9,7 @@ DIR_PATH = os.path.dirname(os.path.realpath(__file__))
 """
 config.json path
 """
-CONFIG_PATH = DIR_PATH + os.sep + 'config.json'
+CONFIG_PATH = DIR_PATH + os.sep + 'resource' + os.sep + 'config' + os.sep + 'config.json'
 
 
 TEST_EXCEL_DIR_PATH = DIR_PATH + os.sep + 'test_excel'
